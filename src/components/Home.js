9f4cf8796dcb1efcfd5a8c24a57664051821dcda
Sonyei <esonyei@gmail.com>
2020-10-21 15:50:37 -0400

    routes working

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function homePage() {
+
+return (
+
+    <div>
+    
+    </div>
+    
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from '../src/components/App';
 import * as serviceWorker from './serviceWorker';
 import { ThemeProvider } from 'styled-components';
 import Theme from './Theme/Theme'
